b1c929282a9d3737967bbcca6d5b572f80962b42
Aashish <aashishanil530@gmail.com>
2024-02-02 19:53:56 +0530

    Routing and GET Request Without Express

--- sample.js
+++ sample.js
@@ -9,19 +9,35 @@
 
 var fs = require("fs");
 var http = require("http");
+var url = require("url");
 http
   .createServer(function (req, res) {
-    if (req.url === "/") {
+    var q = url.parse(req.url,true)
+   console.log( q.pathname)
+
+    if (q.pathname === "/") {
       fs.readFile("sample.html", function (err, data) {
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
         res.end();
-    });
-} else if (req.url === "/login") {
-    res.write("login");
-    res.end();
-} else {
-        res.writeHead(404, { "Content-Type": "text/html" });
+      });
+    } else if (q.pathname === "/login") {
+      fs.readFile("signup.html", (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else if (q.pathname === "/signupaction") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        console.log(q.query)
+        console.log(q.query.fname)
+        console.log(q.query.uname)
+        console.log(q.query.email)
+        console.log(q.query.phone)
+      res.write('<h1>'+q.query.fname+'<h1>');
+      res.end();
+    } else {
+      res.writeHead(404, { "Content-Type": "text/html" });
       res.write("error");
       res.end();
     }
